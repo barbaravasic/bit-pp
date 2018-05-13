26db688e31b942646a9b9701ca2add611efe5b70
Barbara Vasic <barbara.vasic0108@gmail.com>
2018-05-13 18:14:28 +0200

    airport task

---
+++ objects practice/airport/airport.js
@@ -0,0 +1,210 @@
+"use strict";
+
+(function () {
+
+    function Person(name, surname) {
+
+        this.name = name;
+        this.surname = surname;
+
+        this.getData = function () {
+            return name + " " + surname;
+        }
+    }
+
+    function Seat(number, category) {
+        number = number || Math.floor(Math.random() * (100 - 10) + 10);
+        category = category || "e";
+
+        this.number = number;
+        this.category = category.toUpperCase();
+
+        this.getData = function () {
+            return this.number + ", " + this.category;
+        }
+    }
+
+    function Passenger(person, seat) {
+        if (!(person instanceof Person)) {
+            console.log("Insert a valid person");
+            return;
+        } else {
+
+            this.person = person;
+        }
+
+        if (!(seat instanceof Seat)) {
+            console.log("Please insert a valid seat");
+            return;
+        } else {
+            this.seat = seat;
+        }
+
+        function changeSeatCategoryOutput() {
+            if (seat.category === "B") {
+                return seat.category = "bussiness";
+            } else {
+                return seat.category = "economy";
+            }
+        }
+
+        this.getData = function () {
+            return seat.number + ", " + changeSeatCategoryOutput() + ", " + person.getData();
+        }
+    }
+
+    function Flight(relation, date) {
+
+        this.relation = relation;
+        var inputDate = new Date(date);
+        this.date = inputDate.getMonth() + 1 + "." + inputDate.getDate() + "." + inputDate.getFullYear();
+        this.listOfPassengers = [];
+        var counter = 0;
+
+        this.addPassenger = function (passenger) {
+
+            try {
+                for (var i = 0; i < this.listOfPassengers.length; i++) {
+                    if (this.listOfPassengers[i].seat.number === passenger.seat.number)
+
+                        throw passenger.person.name + "'s seat number is already taken"
+                }
+            } catch (err) {
+                console.log(err);
+                passenger.seat.number = Math.floor(Math.random() * (100 - 10) + 10);
+            }
+
+            try {
+                for (var i = 0; i < this.listOfPassengers.length; i++) {
+                    if (passenger.person.getData() === this.listOfPassengers[i].person.getData())
+                        throw "There is already a person with that name on the list"
+                }
+            } catch (err) {
+
+                console.log(err);
+                this.listOfPassengers[i] = passenger;
+            }
+
+            try {
+                this.listOfPassengers.push(passenger);
+                if (this.listOfPassengers.length > 100)
+
+                    throw "There can be only 100 passengers"
+            } catch (err) {
+                console.log(err);
+                this.listOfPassengers.pop();
+            }
+
+        }
+        this.totalInBussiness = function () {
+            this.listOfPassengers.forEach(function (passenger) {
+                if (passenger.seat.category === "bussiness") {
+                    counter++;
+                }
+            })
+            return counter;
+        }
+
+
+
+        this.getData = function () {
+            var output = "";
+            var passOutput = "";
+            this.listOfPassengers.forEach(function (passenger) {
+                passOutput += "\t" + "\t" + passenger.getData() + "\n";
+
+            })
+            output = this.date + ", " + this.relation + ". In bussiness category: " + this.totalInBussiness() + "\n";
+
+            return output + passOutput;
+        }
+    }
+
+    function Airport(name) {
+
+
+        this.name = name;
+        this.listOfFlights = [];
+
+        this.addFlight = function (flight) {
+
+            return this.listOfFlights.push(flight);
+        }
+
+        this.getData = function () {
+            var output = "";
+            var flightOutput = "";
+            var totalNumberOfPassengers = 0;
+            var totalNumberOfPassengersInBussiness = 0;
+
+            this.listOfFlights.forEach(function (flight) {
+                totalNumberOfPassengers += flight.listOfPassengers.length;
+                totalNumberOfPassengersInBussiness += flight.totalInBussiness();
+            })
+            this.listOfFlights.forEach(function (flight) {
+
+                flightOutput += "\t" + flight.getData() + "\n";
+
+            })
+
+            output = "Airport: " + this.name + ", " + "total number of passengers: " + totalNumberOfPassengers + ".Total in bussiness: "+ totalNumberOfPassengersInBussiness + "\n";
+            return output + flightOutput;
+
+        }
+    }
+
+
+    function createFlight(relation, date) {
+        var createdFlight = new Flight(relation, date);
+
+        return createdFlight;
+    }
+
+    function createPassenger(name, surname, seatNumber, category) {
+        var createdPerson = new Person(name, surname);
+        var createdSeat = new Seat(seatNumber, category);
+        var createdPassenger = new Passenger(createdPerson, createdSeat);
+
+        return createdPassenger;
+    }
+
+    // Persons
+    var johnSnow = new Person("John", "Snow");
+    var cercei = new Person("Cercei", "Lannister");
+
+    // Seats
+    var johnseat = new Seat(12, "b");
+    var cerceiseat = new Seat(48, "e");
+
+    // Passengers
+
+    var pass1 = createPassenger("John", "Snow", 1, "b");
+    var pass2 = createPassenger("Cercei", "Lannister", 1, "b");
+    var pass3 = createPassenger("Daenerys", "Targaryen", 14);
+    var pass3 = createPassenger("Daenerys", "Targaryen", 56);
+    var pass4 = createPassenger("Tyrion", "Lannister");
+
+
+    // Flights
+
+    var flight1 = createFlight("Belgrade-Paris", "Oct 25 2017");
+    var flight2 = createFlight("Belgrade-New York", "11.11.2017");
+
+    flight1.addPassenger(pass1);
+    flight1.addPassenger(pass2);
+    flight2.addPassenger(pass3);
+    flight2.addPassenger(pass4);
+
+    flight1.numberOfPassengers = flight1.listOfPassengers.length;
+    flight2.numberOfPassengers = flight2.listOfPassengers.length;
+
+    // Airport
+    var airport1 = new Airport("Nikola Tesla");
+
+    airport1.addFlight(flight1);
+    airport1.addFlight(flight2);
+
+
+    console.log(airport1.getData());
+
+})()
--- objects practice/festival/festival.js
+++ objects practice/festival/festival.js
@@ -3,11 +3,9 @@
 (function () {
 
     function Genre(name) {
-
         this.name = name;
 
         this.getData = function () {
-
             var uppercaseName = this.name.toUpperCase();
             var genreData = uppercaseName[0] + uppercaseName[uppercaseName.length - 1]
             return genreData;
@@ -15,10 +13,16 @@
     }
 
     function Movie(title, genre, length) {
-
         this.title = title;
         this.genre = genre;
-        this.length = length + "min";
+
+        try {
+            if (typeof length === "string") throw "need to insert a number"
+            this.length = length + "min";
+
+        } catch (err) {
+            console.log(err);
+        }
 
         this.getData = function () {
             var movieData = this.title + ", " + this.length + ", " + this.genre.getData();
@@ -34,124 +38,158 @@
         this.totalNumberOfMovies = this.listOfMovies.length;
 
         this.addMovie = function (movie) {
+            var counter = 0;
             if (!movie && !(movie instanceof Movie)) {
                 console.log("Insert a valid movie");
                 return
             }
-            else {
+
+            for (var i = 0; i < this.listOfMovies.length; i++) {
+                if (this.listOfMovies[i].genre.name === movie.genre.name) {
+                    counter++;
+                }
+            }
+
+            if (counter > 3) {
+                console.log("There are already 4 movies that are of the same genre as " + movie.title);
+                return
+
+            } else {
+
                 this.listOfMovies.push(movie);
             }
         }
 
         this.totalLengthOfMovies = function () {
-
             var totalLengthOfMovies = 0;
 
             this.listOfMovies.forEach(function (movie) {
-
                 totalLengthOfMovies += parseInt(movie.length);
             })
 
             return totalLengthOfMovies;
-
         }
 
         this.getData = function () {
-
             var outputString = "";
             var programData = "";
 
             this.listOfMovies.forEach(function (movie) {
 
                 programData += "\t" + movie.getData() + "\n" + "\t";
-
             })
-
             return outputString = this.date + ", " + " program duration " + this.totalLengthOfMovies() + ", " + "\n" + "\t" + programData;
         }
     }
 
-
-    function Festival(name) {
-
+    function Festival(name, maxNum) {
+        var totalNumOfMovies = 0;
+        var numOfMovies = 0;
         this.name = name;
         this.listOfPrograms = [];
+
+        try {
+            if (typeof maxNum === "string") throw "You must insert a number";
+        } catch (err) {
+            console.log(err)
+        }
 
         this.addProgram = function (program) {
             if (!program && !(program instanceof Program)) {
                 console.log("Insert a valid movie");
                 return
             }
-            else {
-                this.listOfPrograms.push(program);
+            
+            if (program.listOfMovies.length + totalNumOfMovies > maxNum) {
+                console.log("You can't have more than " + maxNum + " movies");
+               
+                return
             }
+            this.listOfPrograms.push(program);
+
+            this.listOfPrograms.forEach(function (program) {
+                numOfMovies = program.listOfMovies.length;
+            })
+            totalNumOfMovies += numOfMovies;
+        
         }
 
-
         this.getData = function () {
-
             var outputString = "";
             var festivalData = "";
 
-            this.listOfPrograms.forEach(function (program) {
+            if(this.listOfPrograms.length === 0) {
+                return outputString = "Weekend fetsival" + "\n" + "\t" + "Program to be announced."
+            } 
 
+            this.listOfPrograms.forEach(function (program) {
                 festivalData += program.getData() + "\n" + "\t";
             })
-
-            return outputString = this.name + " has " + this.listOfPrograms.length + " movie titles " + "\n" + "\t" + festivalData;
+            return outputString = this.name + " has " + totalNumOfMovies + " movie titles " + "\n" + "\t" + festivalData;
         }
     }
 
-    function createMovie (title, genre, length) {
-         
+    function createMovie(title, genre, length) {
         var objGenre = new Genre(genre);
         var createdMovie = new Movie(title, objGenre, length);
 
         return createdMovie;
     }
 
-    function createProgram (date) {
-        var createdProgram = new Program (date);
+    function createProgram(date) {
+        var createdProgram = new Program(date);
 
         return createdProgram;
     }
-
 
     // Genres:
     var action = new Genre("action");
     var drama = new Genre("drama");
 
-
     // Movies:
-  
-    var thor2 = createMovie("Thor 2", "action", "120");
-    var loveActually = createMovie("Love Actually", "romance", "100");
+    var thor2 = createMovie("Thor 2", "action", 120);
+    var thor3 = createMovie("Thor 3", "action", 120);
+    var loveActually = createMovie("Love Actually", "romance", 100);
     var psycho = createMovie("Psycho", "horror", "120");
-    var mrBean = createMovie("Mr. Bean", "comedy", "90");
-    var avengers = createMovie("Avengers", "action", "120");
-    var blackPanther= createMovie("Black Panther", "action", "120")
+    var mrBean = createMovie("Mr. Bean", "comedy", 90);
+    var avengers = createMovie("Avengers", "action", 120);
+    var avengers2 = createMovie("Avengers 2", "action", 120);
+    var avengers3 = createMovie("Avengers 3", "action", 120);
+    var blackPanther = createMovie("Black Panther", "action", 120);
+    var highRise = createMovie("High Rise", "drama", 120);
+    var theApartment = createMovie("The Apartment", "romance", 120);
+    var midnight = createMovie("Midnight in Paris", "fantasy", 120);
+    var brooks = createMovie("Mr. Brooks", "thriller", 120);
 
     // Programs:
-
     var dayOne = createProgram("01.05.2018");
     var dayTwo = createProgram("11.05.2018");
 
     dayOne.addMovie(thor2);
     dayOne.addMovie(loveActually);
     dayOne.addMovie(psycho);
+    dayOne.addMovie(avengers2);
+    dayOne.addMovie(avengers3);
+    dayOne.addMovie(blackPanther);
+    dayOne.addMovie(thor3);
+
     dayTwo.addMovie(mrBean);
     dayTwo.addMovie(avengers);
     dayTwo.addMovie(blackPanther);
+    dayTwo.addMovie(highRise);
+    dayTwo.addMovie(theApartment);
+    dayTwo.addMovie(midnight);
+    dayTwo.addMovie(brooks);
 
 
     // Festivals:
-
-    var comicConByBarbaraAndKristina = new Festival("Smotra by Barbara... i Kristina!");
+    var comicConByBarbaraAndKristina = new Festival("Smotra by Barbara... i Kristina!", 10);
+    var fest= new Festival("Festival", 10);
 
     comicConByBarbaraAndKristina.addProgram(dayOne);
     comicConByBarbaraAndKristina.addProgram(dayTwo);
 
 
-    console.log(comicConByBarbaraAndKristina.getData());
+    console.log(fest.getData());
 
-})();
+})()

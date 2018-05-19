94e1d0d06c803f4af4859318cd68751750ee26b3
Barbara Vasic <barbara.vasic0108@gmail.com>
2018-05-19 20:05:11 +0200

    exam1stPart

---
+++ exam project/js/form.js
@@ -0,0 +1,10 @@
+function collectAllFormData(){
+    var chosenSubject = document.querySelector("#choose-subject").value;
+    var nameSurname = document.querySelector(".name-input").value;
+    var grade = document.querySelector(".grade-input");
+
+}
+
+function validateData(nameSurname, grade) {
+    if 
+}
---
+++ exam project/js/subject.js
@@ -0,0 +1,7 @@
+function Subject(name) {
+    this.name = name;
+}
+
+Subject.prototype.getSubjectNAme = function() {
+    return this.name;
+}

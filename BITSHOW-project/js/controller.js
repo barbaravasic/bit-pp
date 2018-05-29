b6807d2e0baf234685127e43959dd8555f7bafa1
BIT Student <classroom@bgit.rs>
2018-05-29 15:45:01 +0200

    za Milicu

--- BITSHOW-project/js/controller.js
+++ BITSHOW-project/js/controller.js
@@ -1,25 +1,27 @@
-const controller = (function (ui, data) {
+import * as ui from './ui_module.js';
+import * as data from './data_module.js';
 
-  function showInfoHandler(event) {
-    if (event.target.parentElement.tagName === "A" || event.target.tagName === "A") {
-      event.preventDefault();
-      data.setLocalStorage(event.target.id);
-      location.href = './show_info.html';
-    }
+function showInfoHandler(event) {
+  if (event.target.parentElement.tagName === "A" || event.target.tagName === "A") {
+    event.preventDefault();
+    console.log(event.target.parentElement.tagName)
+    data.setLocalStorage(event.target.id);
+    location.href = './show_info.html';
   }
-  function searchHandler(event) {
-    const searchValue = ui.$searchInput.val();
-    data.fetchSearchShows(searchValue, ui.displaySearchList, ui.fail);
-  }
+}
 
+function searchHandler(event) {
+  const searchValue = $(`.search-box`).val();
+  data.fetchSearchShows(searchValue, ui.displaySearchList, ui.fail);
+}
+
+
+
+export const init = () => {
+  data.fetchShow(ui.displayTop50, ui.failed);
+  $(`body`).on("click", showInfoHandler);
+  $(`.search-box`).on("keyup", searchHandler);
+  const id = localStorage.getItem("id");
+  data.fetchSeasonsAndCast(id, ui.displayOnShowInfo, ui.failed);
+}
   
-  return {
-    init() {
-      data.fetchShow(ui.displayTop50, ui.failed);
-      $(`body`).on("click", showInfoHandler);
-      $(`.search-box`).on("keyup", searchHandler);
-      const id = localStorage.getItem("id");
-      data.fetchSeasonsAndCast(id, ui.displayOnShowInfo, ui.failed);
-    }
-  }
-})(uiModule, dataModule);
--- BITSHOW-project/js/ui_module.js
+++ BITSHOW-project/js/ui_module.js
@@ -1,14 +1,13 @@
-const uiModule = (function () {
-    return {
 
-        $searchInput: $(`.search-box`),
 
-        displayTop50(list) {
-            const $mainContainer = $(`.main`);
-            const $row = $(`<div class='row'>`);
+// export const $searchInput = $(`.search-box`);
 
-            list.forEach(show => {
-                const $card = $(`
+export const displayTop50 = (list) => {
+    const $mainContainer = $(`.main`);
+    const $row = $(`<div class='row'>`);
+
+    list.forEach(show => {
+        const $card = $(`
                 <div class='col-12 col-md-6 col-lg-4 show' id='${show.id}'>
                     <a href='#'>
                         <img src='${show.posterUrl}' id='${show.id}'>
@@ -16,33 +15,32 @@ const uiModule = (function () {
                     </a>
                 </div>
                 `)
-                $mainContainer.append($row);
-                $row.append($card);
-            })
-        },
+        $mainContainer.append($row);
+        $row.append($card);
+    })
+};
 
-        failed() {
-            alert(`Something went wrong`);
-        },
+export const failed = () => {
+    alert(`Something went wrong`);
+};
 
-        displayOnShowInfo(chosenShow, listOfSeasons, listOfActors) {
-            // const $mainContainer = $(`#show-info-main`).empty();
-            const $mainContainer = $(`#show-info-main`)
-            const $chosenShowTitle = $(`<h3>${chosenShow.name}</h3>`)
-            const $ulSeasons = $(`<ul>`);
-            const $seasonsTitle = $(`<h4 class='showTitle'>`);
-            const $ulACtors = $(`<ul>`);
-            listOfSeasons.forEach(season => {
-                const $li = $(`<li>`);
-                $li.text(`${season.startDate} - ${season.endDate}`);
-                $ulSeasons.append($li);
-            })
-            listOfActors.forEach(actor => {
-                const $liActors = $(`<li>`);
-                $liActors.text(`${actor.name.name}`);
-                $ulACtors.append($liActors);
-            })
-            const $infoDisplay = $(`
+export const displayOnShowInfo = (chosenShow, listOfSeasons, listOfActors) => {
+    const $mainContainer = $(`#show-info-main`)
+    const $chosenShowTitle = $(`<h3>${chosenShow.name}</h3>`)
+    const $ulSeasons = $(`<ul>`);
+    const $seasonsTitle = $(`<h4 class='showTitle'>`);
+    const $ulACtors = $(`<ul>`);
+    listOfSeasons.forEach(season => {
+        const $li = $(`<li>`);
+        $li.text(`${season.startDate} - ${season.endDate}`);
+        $ulSeasons.append($li);
+    })
+    listOfActors.forEach(actor => {
+        const $liActors = $(`<li>`);
+        $liActors.text(`${actor.name.name}`);
+        $ulACtors.append($liActors);
+    })
+    const $infoDisplay = $(`
             <div class='row poster-and-lists'>
                 <div class='col-12 col-md-6'>
                     <img src='${chosenShow.posterUrl}' id='${chosenShow.id}' class='info-img'>
@@ -72,31 +70,31 @@ const uiModule = (function () {
                 </div>
             </div>
             `);
-            $mainContainer.append($chosenShowTitle);
-            $mainContainer.append($infoDisplay);
-        },
+    $mainContainer.append($chosenShowTitle);
+    $mainContainer.append($infoDisplay);
+};
 
-        displaySearchList(listToDisplay) {
-            const $searchDiv = $(`.search-dropdown`);
-            $searchDiv.css("display", "block")
-            const $ulSearch = $(`.dropdown-ul`);
-            $ulSearch.empty();
-            listToDisplay.forEach(show => {
-                const $liSearch = $(`
+export const displaySearchList = (listToDisplay) => {
+    const $searchDiv = $(`.search-dropdown`);
+    $searchDiv.css("display", "block")
+    const $ulSearch = $(`.dropdown-ul`);
+    $ulSearch.empty();
+    listToDisplay.forEach(show => {
+        const $liSearch = $(`
                     <li >
                         <a href='#' id='${show.id}'> ${show.name}</a>
                     </li>
                 `);
 
-                $ulSearch.append($liSearch);
-            })
+        $ulSearch.append($liSearch);
+    })
 
-        },
-        reset() {
-            $(`.search-box`).val("");
-            $(`.dropdown-ul`).empty();
-        },
-    }
+};
+
+export const reset = () => {
+    $(`.search-box`).val("");
+    $(`.dropdown-ul`).empty();
+};
 
 
-})()
+
